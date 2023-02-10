850c4cbe612712d6beacfdfc1b37b8f80d921066
adventin <Andrew-Esin@yandex.ru>
2023-02-10 22:52:48 +0300

    create ShopItemFunc component

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import ShopItemFunc from './components/ShopItemFunc'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+
+const oProductItem = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+}
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={oProductItem} />
+      </div>
+    </div>
+  )
+}
+
+export default App
